081172dab4ff380702ca9a655b3d645db3d7db2a
Your Name <you@example.com>
2022-08-06 00:13:14 +0530

    Initial Commit

--- app/src/main/java/com/bhakti_sangrahalay/kundli/model/KPNakshatraNadiBean.kt
+++ app/src/main/java/com/bhakti_sangrahalay/kundli/model/KPNakshatraNadiBean.kt
@@ -2,5 +2,4 @@ package com.bhakti_sangrahalay.kundli.model
 
 import java.io.Serializable
 
-data class KPNakshatraNadiBean(val planet: String, val starLord: String, val subLord: String) :
-    Serializable
+data class KPNakshatraNadiBean(val planet: String, val starLord: String, val subLord: String) : Serializable
