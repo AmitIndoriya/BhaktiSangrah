41364feea700b0bf0b492065f853c7a25cbdb6ac
Your Name <you@example.com>
2022-08-08 23:28:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/bhakti_sangrahalay/database/dao/BirthDetailInfoDao.kt
@@ -0,0 +1,25 @@
+package com.bhakti_sangrahalay.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.bhakti_sangrahalay.database.entity.BirthDetailInfo
+
+@Dao
+interface BirthDetailInfoDao {
+
+    /* @Query("SELECT * FROM BirthDetailInfo")
+     suspend fun getAll(): List<BirthDetailInfo>*/
+    @Query("SELECT * FROM BirthDetailInfo")
+    fun getAll(): List<BirthDetailInfo>
+
+    /*  @Insert
+      suspend fun insertAll(users: List<BirthDetailInfo>)*/
+    @Insert
+    fun insertAll(users: List<BirthDetailInfo>)
+
+    @Delete
+    suspend fun delete(user: BirthDetailInfo)
+
+}
